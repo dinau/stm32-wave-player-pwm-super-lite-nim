152d73aaece92f5ce4cb635402cbd1ef067eb525
dinau <audgit@gmail.com>
2022-03-21 21:27:01 +0900

    changed: Demo ex

--- src/F4/f401xe/ex/config.nims
+++ src/F4/f401xe/ex/config.nims
@@ -1,3 +1,6 @@
 --path:"."
+
+--path:"../nucleo_f411re"
+
 --passL:"-Wl,-T,STM32F411RETx_FLASH.ld"
 
--- src/F4/f401xe/ex/main.nim
+++ src/F4/f401xe/ex/main.nim
@@ -1,28 +1,33 @@
 import conf_sys
-import system,systick, uart, pwm, gpio, spi
+import system, uart, gpio
 import board
-import regInfo
+import systick
 
 {.compile:"xprintf.c".}
 # set PLL 96MHz for STM32F411
 
 proc main*(){.inline.} = # main function
-    const MSS = 1000000
     initSystem()
     initSysclock()
     initGPIO()
     initBoard()
-    #RCC.AHB1ENR.bset(RCC_AHB1ENR_GPIOAEN_Pos)  # GPIOA clock enable
-    #GPIOA.MODER.bset(GPIO_MODER_MODER5_Pos)
+    # Start systick timer and interrupt
+    SysTick_Config(SYSTEM_CLOCK div 1000) # 1msec (1000Hz)
+    # Set LED port
+    RCC.AHB1ENR.bset(RCC_AHB1ENR_GPIOAEN_Pos)  # GPIOA clock enable
+    GPIOA.MODER.bset(GPIO_MODER_MODER5_Pos)
+
     when UART_INFO:
         initSerial(USART2)                # 115200bps
+
+    var ix = 0
     while true:
-        xprintf("\nhello")
-    while false:
-        for _ in 0..MSS:
-            GPIOA.atomic_out(5,1)
-        for _ in 0..MSS:
-            GPIOA.atomic_out(5,0)
+        xprintf("\nhello :%d",ix)
+        inc(ix)
+        GPIOA.atomic_out(5,1)
+        wait_ms(500)
+        GPIOA.atomic_out(5,0)
+        wait_ms(500)
 
 
 # run main program
